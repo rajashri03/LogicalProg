a9bf16698947002beb89a8f832e4615c9c3f3f57
rajashri03 <telvekar.rajashri@gmail.com>
2023-05-22 09:46:42 +0530

    [Rajashri]-Adding addition code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LogicalProgramming
+{
+    //uc1-addition
+    //uc2-diision
+    //uc3-multiplcation
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition!");
+        }
+    }
+}
